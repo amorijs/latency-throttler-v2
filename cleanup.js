1932165a89d0e0f49fe164da7f899ec0a686f050
Chris Amori <camori@nge.io>
2023-11-01 23:33:08 -0700

    logs in cleanup script

--- cleanup.js
+++ cleanup.js
@@ -20,7 +20,7 @@ const promisifiedExec = function (cmd) {
 const getNetworkInterfaceId = async function () {
   const cached_networkInterfaceId = await promisifiedExec(
     `containerId=$(docker ps --format "{{.ID}} | {{.Names}}" | grep ${process.env.CONTAINER_NAME} | awk '{ print $1 }') && interfaceId=$(docker exec -i "$containerId" cat /sys/class/net/eth0/iflink | sed 's/\\r$//') && ip ad | grep $interfaceId | awk '{ print $2 }' | awk -F@ '{ print $1 }' | grep veth`
-  ).then(output => output.trim())
+  ).then((output) => output.trim())
   console.log('GOT NETWORK ID', cached_networkInterfaceId)
   return cached_networkInterfaceId
 }
@@ -32,3 +32,6 @@ const deleteAllRules = async function () {
 }
 
 deleteAllRules()
+  .then(console.log)
+  .then(() => console.log('done'))
+  .catch(console.error)
